b78c03fbd47bd719ffdf289ff7e416271b53eed3
hongddoni <tmdgur122@naver.com>
2025-07-05 12:59:25 +0900

    feat: react-router-dom 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
 import { ToastProvider } from "./components/toast/ToastContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ToastProvider>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ToastProvider>
 	</StrictMode>
 );
